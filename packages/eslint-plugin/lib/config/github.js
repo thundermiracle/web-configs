6da7d7ead4ee3d284c649a0c81f7b8f193581a68
ThunderMiracle <cissoliu@gmail.com>
2022-04-17 11:34:29 +0900

    feat: add github recommend lint rules (#3)

---
+++ packages/eslint-plugin/lib/config/github.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'plugin:github/recommended',
+
+  rules: {
+    'filenames/match-regex': 'off',
+    'import/no-commonjs': 'off',
+    'import/no-namespace': 'off',
+  },
+};
